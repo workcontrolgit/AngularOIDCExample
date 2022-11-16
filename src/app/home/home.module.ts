657d688bc73672a063daef3fa3341c2e87a29c37
Fuji <fuji.nguyen@workcontrol.com>
2022-11-16 14:20:15 -0500

    Initial check in

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Angulartics2Module } from 'angulartics2';
+
+import { SharedModule } from '@shared';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  imports: [CommonModule, TranslateModule, SharedModule, Angulartics2Module, HomeRoutingModule],
+  declarations: [HomeComponent],
+})
+export class HomeModule {}
