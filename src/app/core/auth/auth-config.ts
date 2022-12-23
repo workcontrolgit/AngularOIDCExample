718e221c4c5a2804d0c04e677d4d5460e3a6aad2
Fuji <fuji.nguyen@workcontrol.com>
2022-12-23 11:50:02 -0500

    refactor toaster component

--- src/app/core/auth/auth-config.ts
+++ src/app/core/auth/auth-config.ts
@@ -11,7 +11,7 @@ export const authConfig: AuthConfig = {
   silentRefreshTimeout: 5000, // For faster testing
   timeoutFactor: 0.25, // For faster testing
   sessionChecksEnabled: true,
-  showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
+  showDebugInformation: false, // Also requires enabling "Verbose" level in devtools
   clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
   nonceStateSeparator: 'semicolon', // Real semicolon gets mangled by Duende ID Server's URI encoding
 };
--- src/app/core/toast/toasts-container.component.ts
+++ src/app/core/toast/toasts-container.component.ts
@@ -8,22 +8,7 @@ import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-toasts',
   standalone: true,
   imports: [NgbToastModule, NgIf, NgTemplateOutlet, NgFor],
-  template: `
-    <ngb-toast
-      *ngFor="let toast of toastService.toasts"
-      [class]="toast.classname"
-      [header]="toast.header"
-      [autohide]="true"
-      [delay]="toast.delay || 5000"
-      (hidden)="toastService.remove(toast)"
-    >
-      <ng-template [ngIf]="isTemplate(toast)" [ngIfElse]="text">
-        <ng-template [ngTemplateOutlet]="toast.textOrTpl"></ng-template>
-      </ng-template>
-
-      <ng-template #text>{{ toast.textOrTpl }}</ng-template>
-    </ngb-toast>
-  `,
+  templateUrl: './toasts-container.component.html',
   host: { class: 'toast-container position-fixed top-0 end-0 p-3', style: 'z-index: 1200' },
 })
 export class ToastsContainer {
